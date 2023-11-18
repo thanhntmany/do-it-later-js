0821e35aae991a372dde129db13508b3e473ab93
Thanhntmany-Laptop <thanhntmany@gmail.com>
2023-11-19 01:02:43 +0700

    update syntax

--- do-it-later--requestAnimationFrame.js
+++ do-it-later--requestAnimationFrame.js
@@ -1,6 +1,7 @@
-class DoItLaterRAF {
+class DoItLater {
     constructor() { this._flush = this.flush.bind(this); this.scheduler = this.queue.bind(this) }
-    queue(fn, args, _this) { this.tail = (this.tail || this).next = { fn: fn, args: args, _this: _this }; if (!this.RAF_ID) this.RAF_ID = requestAnimationFrame(this._flush); }
+    _setter(key, value) { this[key] = value }
+    queue(_this, fn, ...args) { this.tail = (this.tail || this).next = typeof fn === 'function' ? { _this: _this, fn: fn, args: args } : { _this: _this, fn: this._setter, args: [fn, args[0]] }; if (!this.RAF_ID) this.RAF_ID = requestAnimationFrame(this._flush)}
     flush() { var o = this; while (o = o.next) o.fn.apply(o._this, o.args); this.tail = this.next = this.RAF_ID = undefined }
 }
-window.DIL = (new DoItLaterRAF()).scheduler
+window.DIL = (new DoItLater()).scheduler
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 class DoItLater {
-    queue(fn, args, _this) { this.tail = (this.tail || this).next = { fn: fn, args: args, _this: _this } }
+    _setter(key, value) { this[key] = value }
+    queue(_this, fn, ...args) { this.tail = (this.tail || this).next = typeof fn === 'function' ? { _this: _this, fn: fn, args: args } : { _this: _this, fn: this._setter, args: [fn, args[0]] } }
     flush() { var o = this; while (o = o.next) o.fn.apply(o._this, o.args); this.tail = this.next = undefined }
 }
 
